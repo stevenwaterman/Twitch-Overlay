38951aec46354fb1b8932071902cc5a072268357
steven <github@stevenwaterman.uk>
2020-06-05 17:30:29 +0100

    allow changing ip

--- Client/src/root/RootStore.ts
+++ Client/src/root/RootStore.ts
@@ -9,7 +9,7 @@ const rootStore = configureStore({
 export type AppDispatch = typeof rootStore.dispatch
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
-const socket = new WebSocket(`ws://${process.env.REACT_APP_SERVER_IP}:8080/events`);
+const socket = new WebSocket(`ws://${process.env.REACT_APP_SERVER_IP}/events`);
 socket.onmessage = event => {
     const action = JSON.parse(event.data);
     console.log(action);
