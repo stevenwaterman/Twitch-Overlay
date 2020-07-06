b5fc92a2997f10c68e888a479bd0d5d7319a6580
steven <github@stevenwaterman.uk>
2020-07-06 19:14:42 +0100

    Fix raid meows i think

--- Client/src/audio.ts
+++ Client/src/audio.ts
@@ -14,7 +14,6 @@ const paths = [
     "meows/9",
     "meows/10",
     "meows/11",
-    "meows/11",
     "meows/12",
     "meows/13",
     "meows/14",
@@ -32,10 +31,11 @@ type AudioPath = (typeof paths)[number];
 const buffers: Record<AudioPath, AudioBuffer> = {} as any;
 const ctx = new AudioContext()
 paths.forEach(path => {
-    fetch("/sounds/" + path + ".wav").then(res => res.arrayBuffer()).then(res => ctx.decodeAudioData(res)).then(res => buffers[path] = res);
+    fetch("/sounds/" + path + ".wav").then(res => res.arrayBuffer()).then(res => ctx.decodeAudioData(res)).then(res => buffers[path] = res).then(() => console.log("Loaded", path));
 })
 
 export function play(sound: AudioPath, {start, offset, duration, volume, loop, detune}: { start?: number, offset?: number, duration?: number, volume?: number, loop?: boolean, detune?: number } = {}): number {
+    // console.log("playing", sound, start, offset, duration, volume, loop, detune)
     const buffer = buffers[sound];
     if (!buffer) return -1;
 
@@ -56,7 +56,7 @@ export function play(sound: AudioPath, {start, offset, duration, volume, loop, d
     const realStart = start === undefined ? undefined : start + ctx.currentTime;
     sourceNode.start(realStart, offset || 0, duration);
     if (duration !== undefined) {
-        sourceNode.stop((start === undefined ? ctx.currentTime : start) + duration);
+        sourceNode.stop((start || 0) + duration + ctx.currentTime);
     }
     if (loop) return duration || 1 * 1000 * 1000 * 1000;
     return (buffer.duration + (start || 0) - (offset || 0)) * 1000;
