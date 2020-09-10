950b14e4d255c6f7dc15830a6797db7448619edd
steven <github@stevenwaterman.uk>
2020-09-10 17:01:53 +0100

    priority right!

--- Client-Svelte/src/App.svelte
+++ Client-Svelte/src/App.svelte
@@ -34,7 +34,7 @@ export let url = "";
       <Link to="game/single/left">game/single/left</Link>
       <Link to="game/single/right">game/single/right</Link>
       <Link to="game/priority/left">game/priority/left</Link>
-      <Link to="game/priority/left">game/priority/left</Link>
+      <Link to="game/priority/right">game/priority/right</Link>
     </div>
   </Route>
   <Route path="intro" component={IntroWithMusic}/>
@@ -47,7 +47,7 @@ export let url = "";
   <Route path="game/single/left"><SingleGame rightCamera="other"/></Route>
   <Route path="game/single/right"><SingleGame leftCamera="other"/></Route>
   <Route path="game/priority/left"><PriorityGame/></Route>
-  <Route path="game/priority/left"><PriorityGame flipX/></Route>
+  <Route path="game/priority/right"><PriorityGame flipX/></Route>
 </Router>
 
 <!-- <Break/> -->
