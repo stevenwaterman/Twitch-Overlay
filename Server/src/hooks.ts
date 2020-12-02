5b0b1c730a50dcf0aa7351eab8f93f65000a490f
steven <github@stevenwaterman.uk>
2020-12-02 14:33:45 +0000

    Try to fix restart logic

--- Server/src/hooks.ts
+++ Server/src/hooks.ts
@@ -2,6 +2,7 @@ import TwitchClient, {HelixFollow, HelixUser} from "twitch";
 import WebHookListener, {LegacyAdapter} from "twitch-webhooks";
 import {Callbacks, ENV} from "./index";
 import {randomName} from "./utils";
+import FollowsToUserSubscription from "twitch-webhooks/lib/Subscriptions/FollowsToUserSubscription";
 
 export async function initHooks(callbacks: Callbacks, twitchClient: TwitchClient, user: HelixUser, {}: ENV): Promise<Callbacks> {
     const portString: string | undefined = process.env.WEBHOOK_PORT;
@@ -9,14 +10,13 @@ export async function initHooks(callbacks: Callbacks, twitchClient: TwitchClient
     const port = parseInt(portString);
 
     const listener = new WebHookListener(twitchClient, await LegacyAdapter.create({port}));
+    await listener.listen()
 
-    await startHooks(listener);
-    setInterval(() => startHooks(listener), 12 * 3600 * 1000);
-
-    await listener.subscribeToFollowsToUser(user, (follow: HelixFollow) => {
-        console.log("Follow from ", follow.userDisplayName);
-        callbacks.onFollow(follow);
-    });
+    let subscription: FollowsToUserSubscription = await startHooks(listener, user, callbacks);
+    setInterval(async () => {
+        await subscription.stop();
+        subscription = await startHooks(listener, user, callbacks);
+    }, 3600 * 1000); // Hourly
 
     callbacks.debugFollow = () => {
         console.log("Debug Follow");
@@ -32,14 +32,9 @@ export async function initHooks(callbacks: Callbacks, twitchClient: TwitchClient
     return callbacks;
 }
 
-async function startHooks(listener: WebHookListener) {
-    console.log("Starting Hooks");
-
-    try {
-        await listener.unlisten();
-    } catch {}
-
-    await listener.listen();
-
-    console.log("Hooks started");
+async function startHooks(listener: WebHookListener, user: HelixUser, callbacks: Callbacks): Promise<FollowsToUserSubscription> {
+    return await listener.subscribeToFollowsToUser(user, (follow: HelixFollow) => {
+        console.log("Follow from ", follow.userDisplayName);
+        callbacks.onFollow(follow);
+    });
 }
